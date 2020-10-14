59d0198617bfa28fb263d818ebc26d4a90716592
sandwich-420 <68409992+sandwich-420@users.noreply.github.com>
2020-10-14 11:55:51 +0800

    Add files via upload

---
+++ 11.2.php
@@ -0,0 +1,33 @@
+<?php
+/*ID: 602110199
+Name: Cai Anmin(Sandwich)
+Wechat: sandwichi*/
+$pricings = [
+			[ 'unit' => 5,           'price' => 10, 'isWhole' => true ],
+			[ 'unit' => 5,           'price' =>  3, 'isWhole' => false ],
+			[ 'unit' => 5,           'price' =>  5, 'isWhole' => false ],
+			[ 'unit' => PHP_INT_MAX, 'price' => 10, 'isWhole' => false ],
+	        ];
+echo "Input usage unit: ";
+fscanf(STDIN, "%d", $unit);
+class electricity
+{
+	private $pricings;
+	function __construct($par1)
+  {
+    $this->pricings = $par1;
+  }
+	function calculate($unit)
+	{
+		$price = 0;
+		for($i = 0; $unit > 0; $i++) 
+		{
+			$unitCal = ($unit > $this->pricings[$i]['unit'])? $this->pricings[$i]['unit'] : $unit;
+			$price += ($this->pricings[$i]['isWhole'])? $this->pricings[$i]['price'] : $unitCal * $this->pricings[$i]['price'];
+			$unit -= $unitCal;
+		}
+		return $price;
+	}
+}
+$a=new electricity($pricings);
+printf("price of electricity bill = %d\n", $a->calculate($unit));
